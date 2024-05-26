6133422ec5ee9d40087edb0fffb99b77d9ae3abc
MistressNebula <delbovemarie@gmail.com>
2024-05-26 23:06:48 +0200

    Switch to "match highest level" model

--- Data/Script/event_rematch.lua
+++ Data/Script/event_rematch.lua
@@ -4,44 +4,41 @@ require 'common'
 function SINGLE_CHAR_SCRIPT.ApplyRematchBoost(owner, ownerChar, context, args)
     if not SV.guildmaster_summit.rematch then return end
     local player_count = _DATA.Save.ActiveTeam.Players.Count
-    local cubic_total = 0
+    local standard_count = math.min(player_count, 4)
+    local max_level = 0
     for i=0, player_count-1, 1 do
-        cubic_total = cubic_total + _DATA.Save.ActiveTeam.Players[i].Level^3
+        max_level = math.max(_DATA.Save.ActiveTeam.Players[i].Level, max_level)
     end
-    local cubic_average = math.floor(((cubic_total/math.min(player_count, 4))^(1/3)) + 0.01)
-    local boss_level = math.min(math.max(50, cubic_average), 100)
+    local count_multiplier = ((player_count-standard_count)/8)+1
+    local final_count = max_level*count_multiplier + SV.guildmaster_summit.victories
+    if (final_count % 1) >= 0.5
+    then final_count = math.ceil(final_count)
+    else final_count = math.floor(final_count)
+    end
+    local boss_level = math.min(math.max(50, final_count), 100)
+    
 
-    local calcBuff = function(minLevel, maxLevel, minBuff, maxBuff, currentLevel)
+    local calcBuff = function(minLevel, maxLevel, minBuff, buffAtMax, currentLevel)
         if currentLevel<=minLevel then return minBuff end
-        if currentLevel>=maxLevel then return maxBuff end
         local currentInRange = currentLevel-minLevel
         local lvlRange = maxLevel-minLevel
-        local buffRange = maxBuff-minBuff
+        local buffRange = buffAtMax-minBuff
         local buffPerLevel = buffRange/lvlRange
         local buffAtLevel = minBuff + buffPerLevel*currentInRange
-        return math.floor(buffAtLevel, currentInRange)
+        return math.min(math.floor(buffAtLevel), 256)
     end
 
-    local done = {}
     for char in luanet.each(LUA_ENGINE:MakeList(_ZONE.CurrentMap:IterateCharacters(false, true))) do
-        local already_done = false
-        for _, c in pairs(done) do
-            if char:Equals(c) then
-                already_done = true
-                break
-            end
-        end
-        if not already_done then
-            table.insert(done, char)
-            local atkBuff = calcBuff(50, 100, 0,  64, boss_level)
-            local othBuff = calcBuff(50, 100, 64, 96, boss_level) -- defenses, speed
+        if char.Level ~= boss_level then
+            local atkBuff = calcBuff(50, 100, 0,  64,  final_count)
+            local otherBuff = calcBuff(50, 100, 64, 96, final_count)
             char.Level = boss_level
+            char.HP    = char.MaxHP
             char.AtkBonus   = atkBuff
             char.MAtkBonus  = atkBuff
-            char.DefBonus   = othBuff
-            char.MDefBonus  = othBuff
-            char.SpeedBonus = othBuff
-            PrintInfo(tostring(char.Level))
+            char.DefBonus   = otherBuff
+            char.MDefBonus  = otherBuff
+            char.SpeedBonus = otherBuff
         end
     end
 end
